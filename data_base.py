5c7bd7f3befc17c1c2d816cb2530262007723821
Roee Nashchon <roee.nachshon@gmail.com>
2023-05-12 12:08:23 +0300

    first commit

--- data_base.py
+++ data_base.py
@@ -6,17 +6,22 @@ import user
 import time
 import pandas as pd
 import train_ai
+import math
+
+"""df = pickle.load(open("db.pkl", "rb")) # 1100
+pickle.dump(df,open("plan_b_DB.pkl", "wb"))
+df['index'] = (df.index // 100) + 1
+df = df.set_index(['index', df.groupby('index').cumcount()])
+print(df)"""
+
 
 df = pickle.load(open("db.pkl", "rb"))
-y = pd.DataFrame()
-y["before"] = df["Last key-"]
-y["current"] = df["Current key-"]
 
-print(y)
 
 
 X_train, X_test, = train_test_split(df, test_size=0.25, random_state=1)
 clf = OneClassSVM(kernel='rbf')
-clf.fit(X_train,y)
+clf.fit(X_train)
 pickle.dump(clf, open('ai.pkl', 'wb'))
 print("trained!")
+
